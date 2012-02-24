78f0788a0064fbacb8af560a0e68f79b0b3b29e3
Maxime Haineault <max@motion-m.ca>
2012-02-24 13:48:00 -0500

    Updated manage.py for django 1.4

--- djangodukerecipe/manage.py
+++ djangodukerecipe/manage.py
@@ -1,20 +1,18 @@
-from django.core import management
-import os,imp
+import os, sys, imp
 
 def main(settings_module=None):
     if settings_module:
         settings_module = settings_module
-        os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
+       #os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     else:
         settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', 'settings')
     try:
         imp.find_module(settings_module)
     except ImportError, e:
-        import sys
         sys.stderr.write("Error loading the settings module '%s': %s"
                             % (settings_module, e))
         return sys.exit(1)
 
-    
-    utility = management.ManagementUtility()
-    utility.execute()
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
