ac6cc13dfeb31c3d0dae36c15ea3837c14ad2372
h3 <haineault@gmail.com>
2012-04-19 16:02:34 -0300

    Update djangodukerecipe/urls.py

--- djangodukerecipe/urls.py
+++ djangodukerecipe/urls.py
@@ -7,12 +7,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve', 
-      {'document_root': settings.MEDIA_ROOT}),
+
 )
 
 if settings.DEV:
     urlpatterns += patterns('', 
-        (r'^%%s(.*)$' % settings.MEDIA_URL[1:], 
+        (r'^%%s(.*)$' %% settings.MEDIA_URL[1:], 
         'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),)
