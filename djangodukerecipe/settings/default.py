8008aa61954e0dd88e158980a1e9d0bf5364e074
Maxime Barbier <maxime.barbier1991@gmail.com>
2012-06-19 10:30:32 -0300

    Update master

--- djangodukerecipe/settings/default.py
+++ djangodukerecipe/settings/default.py
@@ -114,6 +114,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.core.context_processors.request',
+    #"seoutils.context_processors.meta",
     'django.contrib.messages.context_processors.messages',
 )
 
